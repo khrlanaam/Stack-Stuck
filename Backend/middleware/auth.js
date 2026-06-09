f572c7e5e9d734170e5c948b3fd2df3209db37a1
ferdyPratamaRombell0 <ferdypratama5387@gmail.com>
2026-06-09 13:59:32 +0700

    update frontend & backend

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -3,23 +3,36 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const header = req.headers.authorization;
 
+  console.log("HEADER:", header);
+
   if (!header) {
     return res.status(401).json({
-      message: "Token tidak ada"
+      message: "Token tidak ada",
     });
   }
 
   const token = header.split(" ")[1];
 
+  console.log("TOKEN:", token);
+
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    console.log("DECODED:", decoded);
 
     req.user = decoded;
 
     next();
+
   } catch (err) {
+
+    console.log("JWT ERROR:", err.message);
+
     return res.status(401).json({
-      message: "Token tidak valid"
+      message: "Token tidak valid",
     });
   }
 };
